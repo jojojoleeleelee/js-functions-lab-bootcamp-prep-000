b6b143b555cad47ded6692d4d7f2b69cefbd83b9
Joanne Lee <vernapii@gmail.com>
2018-03-10 07:22:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function happyHolidays () {
   return "Happy holidays!"
 }
 function happyHolidaysTo (name) {
-  return "It\'s ${days} days until ${holiday}!"
+  It\'s ${days} days until ${holiday}!"
 }
