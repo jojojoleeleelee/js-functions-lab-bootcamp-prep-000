0f84a1b631eafa70c9ed15eb2076c47f4ce11884
Joanne Lee <vernapii@gmail.com>
2018-03-10 07:40:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function happyHolidays () {
 }
 function happyHolidaysTo (name) {
  return "Happy holidays, you!"}
- 
- function happyHolidayTo (holiday, name) {
-   returns 'Happy ${holiday}, ${name}!'
- }
- 
- function holidayCountdown (holiday, days) {
-   returns 'It\'s ${days} days until ${holiday}!''
- }
+ //Write a function that takes in multiple parameters
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+//Write a function that takes in multiple parameters
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
