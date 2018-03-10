fdafea50ec141d03ff9f01ce0df65be64d201a94
Joanne Lee <vernapii@gmail.com>
2018-03-10 07:37:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo (name) {
  return "Happy holidays, you!"}
  
  function happyHolidayTo (holiday, name) {
-   returns "Happy ${holiday}, ${name}!"
+   returns 'Happy ${holiday}, ${name}!'
  }
  
  function holidayCountdown (holiday, days) {
