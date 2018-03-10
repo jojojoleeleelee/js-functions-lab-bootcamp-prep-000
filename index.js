2b2ce84fa33c6fa3b565294512a494a83a19a427
Joanne Lee <vernapii@gmail.com>
2018-03-10 07:34:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function happyHolidays () {
 function happyHolidaysTo (name) {
  return "Happy holidays, you!"}
  
+ function happyHolidayTo (holiday, name) {
+   returns "Happy Easter, Jane!"
+ }
  
+ function holidayCountdown (holiday, days) {
+   returns "It's 7 days until Christmas!"
+ }
